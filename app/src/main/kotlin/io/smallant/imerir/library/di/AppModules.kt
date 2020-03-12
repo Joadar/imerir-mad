5e61363ac7dcf36845a2cfd148d08e19a6dcba31
Jonathan Pannetier <jonathan.pannetier@scout24.ch>
2020-03-12 08:33:13 +0100

    Add exercice 7 solution

--- app/src/main/kotlin/io/smallant/imerir/library/di/AppModules.kt
+++ app/src/main/kotlin/io/smallant/imerir/library/di/AppModules.kt
@@ -1,12 +1,16 @@
 package io.smallant.imerir.library.di
 
 import io.smallant.imerir.library.data.BookRepository
+import io.smallant.imerir.library.data.sources.BookLocalDataSource
+import io.smallant.imerir.library.data.sources.BookRemoteDataSource
 import io.smallant.imerir.library.ui.home.HomeViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 private val dataModule = module {
-    // TODO Part XII
+    // TODO Part VII
+    single { BookLocalDataSource() }
+    single { BookRemoteDataSource() }
     single { BookRepository(get(), get()) }
 }
 
